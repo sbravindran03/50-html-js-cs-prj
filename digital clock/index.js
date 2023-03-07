9600c2f7dfd1170d4f1d3d6d90e03ef1497ff571
sbravindran03 <114281707+sbravindran03@users.noreply.github.com>
2023-03-07 20:32:05 +0530

    50projectscsjshtml

---
+++ digital clock/index.js
@@ -0,0 +1,19 @@
+const hourEl = document.getElementById("hour");
+const minEl = document.getElementById("minutes");
+const secEl = document.getElementById("seconds");
+const ampmEl = document.getElementById("ampm ");
+
+
+function UpdateClock() {
+    let h = new Date().getHours()
+    let m = new Date().getMinutes()
+    let s = new Date().getSeconds()
+    let ampm = "AM";
+
+    if (h > 12) {
+
+    }
+
+    return
+
+}
